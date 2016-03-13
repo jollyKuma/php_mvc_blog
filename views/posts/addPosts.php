309c890b6010d242eb37e1b603fe2e908cd9886c
jollyKuma <jollyrilakkuma@gmail.com>
2016-03-13 14:04:56 +0800

    Commit

--- views/posts/addPosts.php
+++ views/posts/addPosts.php
@@ -1,27 +1,21 @@
-<div class="box" style="height:100%; width:100%;margin-top:-1px">
+<div class="box" style="height:100%; width:90%;margin-top:-1px">
   <div id="header">
     <div id="cont-lock"><i class="fa fa-pencil-square-o lock"></i></div>
     <div id="bottom-head"><h1 id="logintoregister">My Blog</h1></div>
   </div> 
    <form action="" method="post">
-   <div>
-    
-    <span><i class="fa fa-camera"></i></span>    
-    
+   <div class="">
         </form>
         <form class="form-inline">
         <div class="form-group">
           <div class="group" >  
-          <textarea class="inputMaterial" id="content" required placeholder="Say Something..." style="width:610px"></textarea>
-            <span class="highlight" style="width:610px"></span>
-            <span class="bar" style="width:610px"></span>
+          <textarea class="inputMaterial" id="content" required placeholder="Say Something..." style="width:530px"></textarea>
+            <span class="highlight" style="width:530px"></span>
+            <span class="bar" style="width:530px"></span>
       </div>
         </div>
-        <div class="form-group">
-          <label class="sr-only" for="exampleInputPassword3">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword3" placeholder="Password">
-        </div>
-        <div class="checkbox">
+        <div class="form-group" style="width: 40px;">
+        <div class="file" ><input type="file" id="img_file" style="opacity: 0" ><span><i class="fa fa-camera"></i></span></div>
         </div>
         <button id="btn_register" type="submit" style="width: 70px;">Post</button>
       </form>
